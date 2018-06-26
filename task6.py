bb2ade06fe4d049f978687fad1f6875036927be6
andreySm27 <35634515+andreySm27@users.noreply.github.com>
2018-06-26 19:06:37 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -4,14 +4,10 @@
 col=input("Count programmer: ");
 if col>10 and col < 16:
 	print(col);print(" програмистов");
-	pass;
-	else :
-		if col%10==1:
-			print(col);print(" програмист");
-		pass;
-		elif col%10>=2 and col%10<=5:
-			print(col);print(" програмиста");
-		pass;
-		else:
-			print(col);print(" програмистов");
-		pass;
+elif col%10==1:
+	print(col);print(" програмист");
+elif col%10>=2 and col%10<=5:
+	print(col);print(" програмиста");
+else:
+	print(col);print(" програмистов");
+pass;
