3f0c4d15e2568f3e1c88b79abd9b419969f999be
andreySm27 <35634515+andreySm27@users.noreply.github.com>
2018-06-26 19:03:36 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -2,12 +2,16 @@
 # -*- coding:utf-8 -*-
 # vim:fileencoding=utf-8
 col=input("Count programmer: ");
-if col%10==1:
-	print(col);print(" програмист");
-	pass;
-elif col%10>=2 and col%10<=5:
-	print(col);print(" програмиста");
-	pass;
-else:
+if col>10 and col < 16:
 	print(col);print(" програмистов");
 	pass;
+	else
+		if col%10==1:
+			print(col);print(" програмист");
+		pass;
+		elif col%10>=2 and col%10<=5:
+			print(col);print(" програмиста");
+		pass;
+		else:
+			print(col);print(" програмистов");
+		pass;
