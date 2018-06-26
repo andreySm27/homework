bcb4abade664ff167ca4711717cd272eef6a1fd3
andreySm27 <35634515+andreySm27@users.noreply.github.com>
2018-06-26 19:17:46 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -6,7 +6,7 @@ if col%100>10 and col%100 < 16:
 	print(col);print(" програмистов");
 elif col%10==1:
 	print(col);print(" програмист");
-elif col%10>=2 and col%10<=5:
+elif col%10>=2 and col%10<5:
 	print(col);print(" програмиста");
 else:
 	print(col);print(" програмистов");
