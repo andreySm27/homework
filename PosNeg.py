0c58739ba8516756fd65ad88b4e66665ca9a7389
andreySm27 <sm.andrey27@gmail.com>
2018-07-01 17:10:18 +0300

    Homework2

---
+++ PosNeg.py
@@ -0,0 +1,8 @@
+#PosNeg
+#If you enter a positive number, the program will write 
+#1 else the result will be -1.
+number = int(input('Any integer: '))	# Input integer
+if number>0:							# If input integer ismore than o
+    print('1')							# Print 1
+else:									# Else
+    print('-1')							# Print -1
---
+++ TakeInput.py
@@ -0,0 +1,6 @@
+#TakeInput
+#This program asks you to keep any text if the text exists, 
+#the program will print out OK, else will not print anything.
+text = input('Any text: ')		# Input text
+if text != '':					# If input text is empty
+    print('OK')					# Print OK
